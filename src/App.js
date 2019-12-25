d9ca55bf4f9bfad509027226420498c5924e2e50
ChampKaiz <59196930+ChampKaiz@users.noreply.github.com>
2019-12-25 08:32:50 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   state = {
     mails: []
   };
+
   componentDidMount() {
     this.setState({ mails: data }); //รับข้อมูล e-mail จากไฟล์ mail.json
   }
@@ -24,23 +25,25 @@ class App extends Component {
         {/*map ข้อมูลใน array ของตัวแปล mails ออกมาทีละชุดๆ*/}
         {this.state.mails.map(mail => (
           <div>
-            <article class="mail row">
-              <div class="circle">
-                {/*วงกลมหน้าเมล */}
-                <h1>{mail.from.name.substring(0, 1)}</h1>
-              </div>
-              <div style={{ marginLeft: "20px" }}>
-                <h5>
-                  {mail.from.name} <br />{" "}
-                </h5>
-                <h5>
-                  {mail.subject} <br />{" "}
-                </h5>
-                <p>
-                  {mail.body} <br />
-                </p>
-              </div>
-            </article>
+            {
+              <article class="mail row">
+                <div class="circle">
+                  {/*วงกลมหน้าเมล */}
+                  <h1>{mail.from.name.substring(0, 1)}</h1>
+                </div>
+                <div style={{ marginLeft: "20px" }}>
+                  <h5>
+                    {mail.from.name} <br />{" "}
+                  </h5>
+                  <h5>
+                    {mail.subject} <br />{" "}
+                  </h5>
+                  <p>
+                    {mail.body} <br />
+                  </p>
+                </div>
+              </article>
+            }
           </div>
         ))}
       </div>
